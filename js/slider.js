f27c31b96a2abafdad3c321bbcf10dc57a4e4777
Lucas Fernandez <lucasm2664789183@gmail.com>
2023-05-25 21:24:33 -0300

    Update slider.js
     jhjkg

--- js/slider.js
+++ js/slider.js
@@ -5,11 +5,7 @@ let sliderSectionLast = sliderSection[sliderSection.length -1];
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 
-const btnLeft2 = document.querySelector("#btn-left2");
-const btnRight2 = document.querySelector("#btn-right2");
 
-const btnLeft3 = document.querySelector("#btn-left3");
-const btnRight3 = document.querySelector("#btn-right3");
 
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
@@ -45,23 +41,7 @@ btnLeft.addEventListener('click', function(){
 });
 
 
-btnRight2.addEventListener('click', function(){
-    Next();
-});
 
-btnLeft2.addEventListener('click', function(){
-    Prev();
-});
-
-
-
-btnRight3.addEventListener('click', function(){
-    Next();
-});
-
-btnLeft3.addEventListener('click', function(){
-    Prev();
-});
 
 
 /*para que sea automatico
