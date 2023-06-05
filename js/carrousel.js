9bdcd1a948b53d7db55c83c16515ef1f35f5375a
Lucas Fernandez <lucasm2664789183@gmail.com>
2023-06-04 23:19:52 -0300

    actualizacion final
    final

---
+++ js/carrousel.js
@@ -0,0 +1,37 @@
+var imagenes = ['./img/img1.jpg','./img/img2.jpg','./img/img3.jpg','./img/img4.jpg','./img/img5.jpg',
+'./img/img6.jpg','./img/img7.jpg','./img/img8.jpg','./img/img9.jpg','./img/img10.jpg','./img/img11.jpg','./img/img12.jpg','./img/img13.jpg','./img/img17.jpg','./img/img18.jpg','./img/img19.jpg','./img/img20.jpg','./img/img21.jpg','./img/img22.jpg','./img/img23.jpg','./img/img24.jpg','./img/img25.jpg','./img/img27.jpg','./img/img28.jpg','./img/img29.jpg','./img/img30.jpg','./img/img31.jpg','./img/img32.jpg','./img/img33.jpg','./img/img34.jpg','./img/img35.jpg','./img/img36.jpg','./img/img37.jpg','./img/img38.jpg','./img/img39.jpg','./img/img40.jpg','./img/img41.jpg','./img/img42.jpg','./img/img43.jpg','./img/img44.jpg','./img/madnes.jpg','./img/video3.mp4'];
+
+var cont = 0;//para llevar un seguimiento de posicion actual
+
+function carrousel(carrouselCont) {//recibe por parametro el contenedor del carousel
+    carrouselCont.addEventListener('click', function(event) {//al hacer clic en el contenedor se ejecuta la funcion
+        var atras = carrouselCont.querySelector('.atras');
+        var adelante = carrouselCont.querySelector('.adelante');
+        var img = carrouselCont.querySelector('img');
+        var tgt = event.target;//se guarda la referencia al elemento en el que se hizo clic.
+
+        //verifica si el objetivo del evento es atras o adelante
+        if (tgt === atras) {
+            if (cont > 0) {
+                img.src = imagenes[cont - 1];
+                cont--;
+            } else {
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
+            }
+        } else if (tgt === adelante) {
+            if (cont < imagenes.length - 1) {
+                img.src = imagenes[cont + 1];
+                cont++;
+            } else {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var carrouselCont = document.querySelector('.carrouselCont');
+    carrousel(carrouselCont);
+});
--- js/foroForm.js
+++ js/foroForm.js
@@ -1,82 +1,55 @@
 
 function verificarForm() {
+    let error_msg = false;
     let reg_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
     let nombre = document.getElementById("nombre");
     let correo = document.getElementById("correo");
-    let txtarea = document.getElementById("txtarea")
-   // let form = document.getElementById("form");
+    let txtarea = document.getElementById("mensaje")
+    // let form = document.getElementById("form");
     let warning = document.getElementById("warning");
     let dev = document.getElementById("dev");
-    console.log("linea 10 " + dev.firstChild);
-//    while (dev.firstChild) {
-//        dev.removeChild(div.firstChild);
-//    }
+    while (warning.firstChild) {
+        warning.removeChild(warning.firstChild);
+    }
     console.log("nombre " + nombre.value);
     console.log("correo " + correo.value);
-    console.log("txtarea " + txtarea.value); 
+    console.log("txtarea " + txtarea.value);
     if (nombre.value.trim() == "") {
         let error = document.createElement("p");
         error.innerHTML = "Nombre no puede estar vacio";
-        warning.appendChild(error); 
-        console.log("LINEA 17");
-    }else{
-        console.log("LINEA 19");
+        warning.appendChild(error);
+        error_msg = true;
     }
-    if (correo.value.trim() !="" && !reg_email.test(correo.value) ) {
+
+    if (correo.value.trim() != "" && !reg_email.test(correo.value)) {
         let error = document.createElement("p");
         error.innerHTML = "El email es incorrecto, INVALIDO!!";
-        warning.appendChild(error); 
-        console.log("LINEA 26");
+        warning.appendChild(error);
+        error_msg = true;
     }
-    if (txtarea.value.trim() =="" ) {
+        
+    if (txtarea.value.trim() == "") {
         let error = document.createElement("p");
-        error.innerHTML = "Es obligatorio !!";
-        warning.appendChild(error); 
-        console.log("LINEA 32");
+        error.innerHTML = "Mensaje Es obligatorio !!";
+        warning.appendChild(error);
+        error_msg = true;
     }
-    if (txtarea.value.length >200 ) {
+    if (txtarea.value.length > 200) {
         let error = document.createElement("p");
         error.innerHTML = "Supero el maximo de caracteres permitidos, sea mas breve!!";
-        warning.appendChild(error); 
-        console.log("LINEA 32");
+        warning.appendChild(error);
+        error_msg = true;
     }
-
-
+    if (!error_msg) {
+        let msg_gral = document.createElement("p");
+        msg_gral.innerHTML = " NOMBRE: " + nombre.value;
+        dev.appendChild(msg_gral);
+        msg_gral = document.createElement("p");
+       msg_gral.innerHTML = " EMAIL: " + correo.value;
+        dev.appendChild(msg_gral);
+        msg_gral = document.createElement("p");
+        msg_gral.innerHTML = " MENSAJE: " + txtarea.value;
+            dev.appendChild(msg_gral);
+    }
     return false;
 }
-// form.addEventListener("submit", e=> {
-//     e.preventDefault();
-//     let warnings = "";
-//     let datos = "";
-//     let inn = false;
-//     parrafo.innerHTML = "";
-//     let reg_Email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-//     if (nombre.value.length < 6) {
-//         warnings += "El nombre es invalido.<br>";
-//         inn = true;
-//     }
-//     if (!reg_Email.test(correo.value)) {
-//         warnings += "Email invalido.<br>";
-//         inn = true;
-//     }
-      
-//     if (txt.value.length > 300) {
-//         warnings += "Usuario invalido.<br>";
-//         inn = true;
-//     }
-
-//     if (inn) {
-//         parrafo.innerHTML = warnings;
-//     }
-//     else {
-//         datos += "Nombre: " + nombre.value + "<br>";
-//         datos += "Correo: " + correo.value + "<br>";
-//         datos += "Teléfono: " + telefono.value + "<br>";
-//         datos += "Usuario: " + usuario.value + "<br>";
-
-//         parrafo.innerHTML = "Enviado<br><br>Datos ingresados:<br>" + datos;
-//         form.reset();
-//     }
-    
-
-// })
